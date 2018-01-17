8e39f3fd3537fb18406aed2303f8966be170045e
Azzorg <c.goupil72@gmail.com>
2018-01-17 08:53:38 -0500

    Ajout plugin

--- myApp/src/app/app.component.ts
+++ myApp/src/app/app.component.ts
@@ -6,6 +6,8 @@ import { SplashScreen } from '@ionic-native/splash-screen';
 import { HomePage } from '../pages/home/home';
 import { ListPage } from '../pages/list/list';
 
+
+
 @Component({
   templateUrl: 'app.html'
 })
@@ -21,8 +23,10 @@ export class MyApp {
 
     // used for an example of ngFor and navigation
     this.pages = [
-      { title: 'Home', component: HomePage },
-      { title: 'List', component: ListPage }
+      { title: 'Accueil', component: HomePage },
+      { title: 'Équipe', component: HomePage },
+      { title: 'Calendrier', component: HomePage },
+      { title: 'Statistiques', component: ListPage }
     ];
 
   }
--- myApp/src/app/app.module.ts
+++ myApp/src/app/app.module.ts
@@ -2,18 +2,26 @@ import { BrowserModule } from '@angular/platform-browser';
 import { ErrorHandler, NgModule } from '@angular/core';
 import { IonicApp, IonicErrorHandler, IonicModule } from 'ionic-angular';
 
+/* Pages */ 
 import { MyApp } from './app.component';
 import { HomePage } from '../pages/home/home';
 import { ListPage } from '../pages/list/list';
 
+/* Models */
+import { PlayerModel } from '../models/PlayerModel';
+import { MatchModel } from '../models/MatchModel';
+
 import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
+import { ParserbddProvider } from '../providers/parserbdd/parserbdd';
 
 @NgModule({
   declarations: [
     MyApp,
     HomePage,
-    ListPage
+    ListPage,
+    PlayerModel,
+    MatchModel
   ],
   imports: [
     BrowserModule,
@@ -23,12 +31,15 @@ import { SplashScreen } from '@ionic-native/splash-screen';
   entryComponents: [
     MyApp,
     HomePage,
-    ListPage
+    ListPage,
+    PlayerModel,
+    MatchModel
   ],
   providers: [
     StatusBar,
     SplashScreen,
-    {provide: ErrorHandler, useClass: IonicErrorHandler}
+    { provide: ErrorHandler, useClass: IonicErrorHandler },
+    ParserbddProvider
   ]
 })
-export class AppModule {}
+export class AppModule { }
