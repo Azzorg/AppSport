1955821c03522616bb35911033fd18aed67327d6
Azzorg <c.goupil72@gmail.com>
2018-01-22 20:11:42 -0500

    Ajout module router au projet

--- myApp/src/app/app.module.ts
+++ myApp/src/app/app.module.ts
@@ -1,6 +1,9 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { ErrorHandler, NgModule } from '@angular/core';
 import { IonicApp, IonicErrorHandler, IonicModule } from 'ionic-angular';
+import { HttpModule } from '@angular/http';
+import { RouterModule, Routes } from '@angular/router';
+import { HttpClientModule } from '@angular/common/http';
 
 /* Pages */
 import { MyApp } from './app.component';
@@ -16,6 +19,7 @@ import { MatchModel } from '../models/MatchModel';
 import { StatusBar } from '@ionic-native/status-bar';
 import { SplashScreen } from '@ionic-native/splash-screen';
 import { ParserbddProvider } from '../providers/parserbdd/parserbdd';
+
 
 @NgModule({
   declarations: [
